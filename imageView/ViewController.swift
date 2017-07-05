4a15794099f6547bc1a61a3af7ac50b01a08a5ad
quanmacos <quanmacos@quanmacoss-Mac.local>
2017-07-05 12:39:33 +0700

    nop bai

--- imageView/ViewController.swift
+++ imageView/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtURL: UITextField!
+    @IBOutlet weak var imgVew: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btnLoadClick(_ sender: Any) {
+        if txtURL.text != "" {
+            LoadImage(url: txtURL.text!)
+        }
+    }
+    
+    func LoadImage(url: String) {
+        let url: URL = URL(string: url)!
+        
+        (URLSession(configuration: .default)).dataTask(with: url, completionHandler: { (data, respone, error) in
+            if let imageData = data {
+                DispatchQueue.main.async { () -> Void in
+                    self.imgVew.image = UIImage(data: imageData)
+                }
+            } else {
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(action)
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }).resume()
+    }
 }
-
